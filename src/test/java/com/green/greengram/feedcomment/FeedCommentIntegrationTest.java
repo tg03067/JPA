090c701ad8d777264334ea0466fd592c00c0a366
tg03067 <163490444+tg03067@users.noreply.github.com>
2024-06-13 17:57:09 +0900

    ver2 Tdd UserFollowIntegrationTest ver2

---
+++ src/test/java/com/green/greengram/feedcomment/FeedCommentIntegrationTest.java
@@ -0,0 +1,41 @@
+package com.green.greengram.feedcomment;
+
+import com.green.greengram.BaseIntegrationTest;
+import com.green.greengram.common.model.ResultDto;
+import com.green.greengram.feedcomment.model.FeedCommentPostReq;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class FeedCommentIntegrationTest extends BaseIntegrationTest {
+    private final String BASE_URL = "/api/feed/comment";
+
+    @Test
+    @Rollback
+    @DisplayName("feed-comment-post 댓글쓰기")
+    public void testFeedCommentPost() throws Exception {
+        FeedCommentPostReq p = new FeedCommentPostReq();
+        p.setFeedId(11);
+        p.setUserId(1);
+        p.setComment("내용1");
+        p.setFeedCommentId(1);
+        String reqJson = om.writeValueAsString(p);
+
+        MvcResult mr = mvc.perform(post(BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(reqJson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String resJson = mr.getResponse().getContentAsString();
+        ResultDto<Integer> result = om.readValue(resJson, ResultDto.class);
+        assertEquals(1, p.getFeedCommentId(), "먼가 이상");
+    }
+}
---
+++ src/test/java/com/green/greengram/feedfavorite/FeedFavoriteIntegrationTest.java
@@ -0,0 +1,60 @@
+package com.green.greengram.feedfavorite;
+
+import com.green.greengram.BaseIntegrationTest;
+import com.green.greengram.common.model.ResultDto;
+import com.green.greengram.feedfavorite.model.FeedFavoriteToggleReq;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class FeedFavoriteIntegrationTest extends BaseIntegrationTest {
+    private final String BASE_URL = "/api/feed/favorite";
+    @Test
+    @Rollback(value = false)
+    @DisplayName("favorite-toggle-delete 좋아요 취소 토글")
+    public void favoriteToggle() throws Exception {
+        FeedFavoriteToggleReq p = new FeedFavoriteToggleReq(5,1);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("feed_id", String.valueOf(p.getFeedId()));
+        params.add("user_id", String.valueOf(p.getUserId()));
+
+        MvcResult mr = mvc.perform(get(BASE_URL).params(params))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String resJson = mr.getResponse().getContentAsString();
+        ResultDto<Integer> result = om.readValue(resJson, ResultDto.class);
+        assertEquals(0, result.getResultData());
+        assertEquals("좋아요 취소", result.getResultMsg());
+    }
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("favorite-toggle-post 좋아요 처리 토글")
+    public void favoriteToggle2() throws Exception {
+        FeedFavoriteToggleReq p = new FeedFavoriteToggleReq(6,1);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("feed_id", String.valueOf(p.getFeedId()));
+        params.add("user_id", String.valueOf(p.getUserId()));
+
+        MvcResult mr = mvc.perform(get(BASE_URL).params(params))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String resJson = mr.getResponse().getContentAsString();
+        ResultDto<Integer> result = om.readValue(resJson, ResultDto.class);
+        assertEquals(1, result.getResultData());
+        assertEquals("좋아요 처리", result.getResultMsg());
+    }
+}
